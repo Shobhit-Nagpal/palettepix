14bc5ea0dcff4afafaa95d1ad895a72dc80957bb
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-07-18 15:30:57 +0530

    Formatted content

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,10 @@ const About = () => {
             </div>
 
             <div className="about__content">
-                <p style={ {color: textColor} }>PalettePix is a place where designers and developers can quickly test out color palettes to get an idea of how their websites would look.It is designed to help you explore and experiment with different color combinations for your creative projects. You can mix and match hues, explore various palettes, and find inspiration from trending color schemes.</p>
+                <p style={ {color: textColor} }>PalettePix is a place where designers and developers can quickly test out color palettes to get an idea of how their websites would look. It is designed to help you explore and experiment with different color combinations for your creative projects.
+                    <br />
+                    <br />
+                    You can mix and match hues, explore various palettes, and find inspiration from trending color schemes. Happy exploring!</p>
             </div>
 
         </div>
