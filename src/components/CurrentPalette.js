d4c19b361284b66d491b3bc4e05fe002a20fbf62
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-07-18 16:29:23 +0530

    Added info and copy to clipboard

--- src/components/CurrentPalette.js
+++ src/components/CurrentPalette.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useColor } from "../context/ColorContext";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import "../styles/CurrentPalette.css";
 
 const CurrentPalette = () => {
@@ -17,6 +18,28 @@ const CurrentPalette = () => {
                 <div style={ {backgroundColor: secondaryColor, borderWidth:'1px', borderStyle:'solid', borderColor: textColor, width: '5rem', height: '3rem'} }></div>
                 <div style={ {backgroundColor: tertiaryColor, borderWidth:'1px', borderStyle:'solid', borderColor: textColor, width: '2rem', height: '3rem'} }></div>
                 <div style={ {backgroundColor: textColor, borderWidth:'1px', borderStyle:'solid', borderColor: textColor, width: '1rem', height: '3rem'} }></div>
+            </div>
+
+            <div className="current_palette_info_container">
+                <div className="current_palette_info">
+                    <h3 style={ {color: textColor} }>Primary: {primaryColor}</h3>
+                    <ContentCopyIcon style={ {fill:textColor} } onClick={() => navigator.clipboard.writeText(primaryColor)} />
+                </div>
+
+                <div className="current_palette_info">
+                    <h3 style={ {color: textColor} }>Secondary: {secondaryColor}</h3>
+                    <ContentCopyIcon style={ {fill: textColor} } onClick={() => navigator.clipboard.writeText(secondaryColor)} />
+                </div>
+
+                <div className="current_palette_info">
+                    <h3 style={ {color: textColor} }>Tertiary: {tertiaryColor}</h3>
+                    <ContentCopyIcon style={ {fill: textColor} } onClick={() => navigator.clipboard.writeText(tertiaryColor)} />
+                </div>
+
+                <div className="current_palette_info">
+                    <h3 style={ {color: textColor} }>Text: {textColor}</h3>
+                    <ContentCopyIcon style={ {fill: textColor} } onClick={() => navigator.clipboard.writeText(textColor)} />
+                </div>
             </div>
 
 
--- src/context/ColorContext.js
+++ src/context/ColorContext.js
@@ -9,9 +9,9 @@ const useColor = () => {
 const ColorProvider = ( {children} ) => {
  
     const [primaryColor, setPrimaryColor] = useState("#121212");
-    const [secondaryColor, setSecondaryColor] = useState("#8b19d1");
+    const [secondaryColor, setSecondaryColor] = useState("#1A8B19");
     const [tertiaryColor, setTertiaryColor] = useState("#8B19D1");
-    const [textColor, setTextColor] = useState("#ffffff");
+    const [textColor, setTextColor] = useState("#FFFFFF");
 
     return (
         <ColorContext.Provider value={{primaryColor, secondaryColor, tertiaryColor, textColor, setPrimaryColor, setSecondaryColor, setTertiaryColor, setTextColor}}>
