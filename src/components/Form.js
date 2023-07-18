8ed212e715d9260c91eed9b7f8d0390035be3373
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-07-18 09:48:57 +0530

    Changed method for taking form input

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,63 +7,41 @@ const Form = (props) => {
     const {setToggleForm} = props;
     const {primaryColor, secondaryColor, tertiaryColor, textColor, setPrimaryColor, setSecondaryColor, setTertiaryColor, setTextColor} = useColor();
 
-    const changeColors = () => {
-        const primColor = document.getElementById('primary_color');
-        const secColor = document.getElementById('secondary_color');
-        const tertColor = document.getElementById('tertiary_color');
-        const txtColor = document.getElementById('text_color');
-
-        const colorInputRegex = /^(#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})|[a-zA-Z]+)$/;
-
-        const colors = [primColor, secColor, tertColor, txtColor];
-
-        for (let i = 0 ; i < colors.length ; i++) {
-            const color = colors[i];
-            if (!colorInputRegex.test(color.value)) {
-                alert(`Please check your input for the ${color.name}`);
-                return;
-            } 
-        }
-
-        setPrimaryColor(primColor.value.toLowerCase());
-        setSecondaryColor(secColor.value.toLowerCase());
-        setTertiaryColor(tertColor.value.toLowerCase());
-        setTextColor(txtColor.value.toLowerCase());
-
-        setToggleForm(false);
-    }
-    return (
+   return (
         <div className="form__container">
             <h1 className="form__container__header">Pick your colors</h1>
             <label htmlFor="primary_color">Primary Color</label> <br />
-            <input type="text" id="primary_color" name="Primary Color" defaultValue={primaryColor}></input>
+            <input type="text" id="primary_color" name="Primary Color" value={primaryColor} onChange={e => setPrimaryColor(e.target.value)}></input>
+            <input type="color" id="primary_color_picker" value={primaryColor} onChange={e => setPrimaryColor(e.target.value)}></input>
 
             <br />
             <br />
         
             <label htmlFor="secondary_color">Secondary Color</label> <br />
-            <input type="text" id="secondary_color" name="Secondary Color" defaultValue={secondaryColor}></input>
+            <input type="text" id="secondary_color" name="Secondary Color" value={secondaryColor} onChange={e => setSecondaryColor(e.target.value)}></input>
+            <input type="color" id="secondary_color_picker" value={secondaryColor} onChange={e => setSecondaryColor(e.target.value)}></input>
 
             <br />
             <br />
         
 
             <label htmlFor="tertiary_color">Tertiary Color</label> <br />
-            <input type="text" id="tertiary_color" name="Tertiary Color" defaultValue={tertiaryColor}></input>
+            <input type="text" id="tertiary_color" name="Tertiary Color" value={tertiaryColor} onChange={e => setTertiaryColor(e.target.value)}></input>
+            <input type="color" id="tertiary_color_picker" value={tertiaryColor} onChange={e => setTertiaryColor(e.target.value)}></input>
 
             <br />
             <br />
         
 
             <label htmlFor="text_color">Text color</label> <br />
-            <input type="text" id="text_color" name="Text color" defaultValue={textColor}></input>
+            <input type="text" id="text_color" name="Text color" value={textColor} onChange={e => setTextColor(e.target.value)}></input>
+            <input type="color" id="tertiary_color_picker" value={textColor} onChange={e => setTextColor(e.target.value)}></input>
 
             <br />
             <br />
         
 
-            <button className="form__container__changeBtn" onClick={() => changeColors()}>Change color</button>
-            <button className="form__container__cancelBtn" onClick={() => setToggleForm(false)}>Cancel</button>
+            <button className="form__container__changeBtn" onClick={() => setToggleForm(false)} style={ {backgroundColor:'#8B19D1'} }>Close</button>
         </div>
     )
 }
